54ab93708ce76372a256b5189b36d97fd07c87a3
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-26 14:59:00 -0700

    Users can now successfully log_in to the website with authentication, also adds view for users/new

--- skeleton/app/controllers/users_controller.rb
+++ skeleton/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_current_user!, except: [:create, :new]
-  
+  # before_action :require_current_user!, except: [:create, :new]
+
   def create
     @user = User.new(user_params)
     if @user.save
