2c4dd1444463aeacdf943251841cf4617dff4f00
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-26 10:50:06 -0700

    Adds authenticatiom for User model

--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -15,5 +15,38 @@ class User < ApplicationRecord
   validates :password_digest, presence: {message: "Password can\'t be blank"}
   validates :session_token, presence: true, uniqueness: true
   validates :Password, length: {minimum: 6, allow_nil: true}
-  
+  after_initialize :ensure_session_token
+
+  attr_rader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return user if user && is_passowrd?(password)
+    nil
+  end
+
+  def User.reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_passowrd?(password)
+    BCrypt::Password.new(user.password_digest)is_passowrd?(password)
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
 end
